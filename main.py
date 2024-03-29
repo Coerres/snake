3a07236e6eb5b9990e6873756d4541408487feb2
Carina Cordes <carina.cordes@ksb-sg.ch>
2024-03-29 15:06:02 +0100

    Update 'game_over' function

--- main.py
+++ main.py
@@ -121,7 +121,10 @@ def check_collisions(snake):
     
 
 def game_over():
-    pass
+
+    canvas.delete(ALL)
+    canvas.create_text(canvas.winfo_width()/2, canvas.winfo_height()/2,
+                       font=('consolas', 70), text= "GAME OVER", fill="red", tag="gameover")
 
 
 
