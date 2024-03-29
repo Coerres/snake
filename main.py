38836f1e3480f4e2c07ad772e4b8d4a6264293a1
Carina Cordes <carina.cordes@ksb-sg.ch>
2024-03-29 14:08:08 +0100

    Add attributes

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from tkinter import *
 import random
 
-
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#03fc7f"
+FOOD_COLOR = "#fc035a"
+BACKGROUND_COLOR = "#2e2e2c"
 
 
 class Snake:
